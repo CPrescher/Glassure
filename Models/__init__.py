47b73c5822e3ae11a0225dc5527410961fd97bd8
Clemens Prescher <clemens.prescher@gmail.com>
2014-10-30 14:13:07 -0500

    Working on ScatteringFactors

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,2 +1,17 @@
 # -*- coding: utf8 -*-
 __author__ = 'Clemens Prescher'
+
+import sys
+import os
+
+
+def we_are_frozen():
+    # All of the modules are built-in to the interpreter, e.g., by py2exe
+    return hasattr(sys, "frozen")
+
+
+def module_path():
+    encoding = sys.getfilesystemencoding()
+    if we_are_frozen():
+        return os.path.dirname(unicode(sys.executable, encoding))
+    return os.path.dirname(unicode(__file__, encoding))
