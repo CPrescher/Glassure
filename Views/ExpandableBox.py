f59739434363ee4bec58437ea02af2e194d05b72
Clemens Prescher <clemens.prescher@gmail.com>
2015-01-02 15:48:27 -0600

    Refactoring: Reorganization in small functions

--- Views/ExpandableBox.py
+++ Views/ExpandableBox.py
@@ -10,46 +10,55 @@ class ExpandableBox(QtGui.QWidget):
         self._vlayout = QtGui.QVBoxLayout()
         self._vlayout.setContentsMargins(0, 0, 0, 0)
         self._vlayout.setSpacing(0)
+
+        self.create_head_widget(title)
+        self.create_content_widget(content_widget)
+        self.set_custom_stylesheet()
+
+        self._vlayout.addWidget(self.head_widget)
+        self._vlayout.addWidget(self.content_widget)
+        self.setLayout(self._vlayout)
+
+        self.minimize_btn.clicked.connect(self.change_visible_state)
+
+        self.minimized = False
+
+    def create_head_widget(self, title):
         self._head_layout = QtGui.QHBoxLayout()
         self._head_layout.setContentsMargins(10, 8, 15, 0)
         self._head_layout.setSpacing(0)
-
 
         self.minimize_btn = QtGui.QPushButton("-")
         self.minimize_btn.setFixedHeight(20)
         self.minimize_btn.setFixedWidth(20)
         self.minimize_btn.setObjectName("minimize_btn")
 
-        self._head_layout.addWidget(self.minimize_btn)
-
-
-        self._head_layout.addSpacing(10)
         self.title_lbl = QtGui.QLabel(title)
         self.title_lbl.setStyleSheet("font: italic 15px;")
+
+        self._head_layout.addWidget(self.minimize_btn)
+        self._head_layout.addSpacing(10)
         self._head_layout.addWidget(self.title_lbl)
 
-        self.minimized = False
-
-        self.content_widget = content_widget
         self.head_widget = QtGui.QWidget()
         self.head_widget.setLayout(self._head_layout)
         self.head_widget.setObjectName("head_widget")
 
-        self._vlayout.addWidget(self.head_widget)
 
+    def create_content_widget(self, content_widget):
 
-        self._content_widget = QtGui.QWidget()
-        self._content_layout = QtGui.QVBoxLayout()
-        self._content_layout.setContentsMargins(8,8,8,8)
-        self._content_layout.setSpacing(0)
-        self._content_layout.addWidget(self.content_widget)
-        self._content_widget.setLayout(self._content_layout)
+        self._content_widget = content_widget
 
-        self._vlayout.addWidget(self._content_widget)
-        self._content_widget.setObjectName("content_widget")
+        self.content_widget = QtGui.QWidget()
+        self.content_layout = QtGui.QVBoxLayout()
+        self.content_layout.setContentsMargins(8,8,8,8)
+        self.content_layout.setSpacing(0)
+        self.content_layout.addWidget(self._content_widget)
+        self.content_widget.setLayout(self.content_layout)
 
-        self.setLayout(self._vlayout)
+        self.content_widget.setObjectName("content_widget")
 
+    def set_custom_stylesheet(self):
         self.setStyleSheet(
             """
             QLabel, #head_widget{
@@ -63,7 +72,7 @@ class ExpandableBox(QtGui.QWidget):
             }
             """
         )
-        self._content_widget.setStyleSheet(
+        self.content_widget.setStyleSheet(
             """
             QLabel, QGroupBox, #content_widget{
                 background: #3B3B3B;
@@ -89,14 +98,12 @@ class ExpandableBox(QtGui.QWidget):
             """
         )
 
-        self.minimize_btn.clicked.connect(self.change_state)
-
-    def change_state(self):
+    def change_visible_state(self):
         if self.minimized:
-            self._content_widget.show()
+            self.content_widget.show()
             self.minimized = False
             self.minimize_btn.setText("-")
         else:
-            self._content_widget.hide()
+            self.content_widget.hide()
             self.minimized = True
             self.minimize_btn.setText("+")
