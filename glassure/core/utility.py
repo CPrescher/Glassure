533b7e6107ee7e191ea1e96ced24faa701c97e29
Clemens Prescher <clemens.prescher@gmail.com>
2015-07-13 22:56:57 -0500

    updated the doc strings for utility functions

--- glassure/core/utility.py
+++ glassure/core/utility.py
@@ -18,9 +18,9 @@ __all__ = ['calculate_f_mean_squared', 'calculate_f_squared_mean', 'calculate_in
 
 def calculate_f_mean_squared(composition, q):
     """
-    calculates <f>^2 as defined in Waseda book
+    Calculates the square of the mean form_factor for a given composition over q.
     :param composition: dictionary with elements as key and abundances as relative numbers
-    :return:
+    :param q: Q value or numpy array with a unit of A^-1
     """
     norm_elemental_abundances = normalize_composition(composition)
 
@@ -32,9 +32,9 @@ def calculate_f_mean_squared(composition, q):
 
 def calculate_f_squared_mean(composition, q):
     """
-    calculates <f^2> as defined in Waseda book
+    Calculates the mean of the squared form factors for a given composition for a given q vector.
     :param composition: dictionary with elements as key and abundances as relative numbers
-    :return:
+    :param q: Q value or numpy array with a unit of A^-1
     """
     norm_elemental_abundances = normalize_composition(composition)
 
@@ -46,10 +46,9 @@ def calculate_f_squared_mean(composition, q):
 
 def calculate_incoherent_scattering(composition, q):
     """
-    Calculates compton/incoherent scattering for a compound
+    Calculates compton/incoherent scattering for a given composition
     :param composition: dictionary with elements as key and abundances as relative numbers
-    :param q: q_values in reverse Angstrom
-    :return: ndarray of compton scattering
+    :param q: Q value or numpy array with a unit of A^-1
     """
     norm_elemental_abundances = normalize_composition(composition)
 
@@ -79,7 +78,7 @@ def normalize_composition(composition):
 
 def convert_density_to_atoms_per_cubic_angstrom(composition, density):
     """
-    Converts densities in g/cm3 into atoms per A^3
+    Converts densities given in g/cm3 into atoms per A^3
     :param composition: dictionary with elements as key and abundances as relative numbers
     :param density: density in g/cm^3
     :return: density in atoms/A^3
