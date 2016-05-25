4b3da790e2d46d64c39bbf510d75533e9b89ff50
Clemens Prescher <clemens.prescher@gmail.com>
2016-05-25 10:39:50 +0200

    fixing bug in test

--- glassure/tests/old/test_GlassureModel.py
+++ glassure/tests/old/test_GlassureModel.py
@@ -54,7 +54,8 @@ class GlassureModelTest(unittest.TestCase):
         r = np.linspace(0, 10, 1000)
 
         self.model.background_scaling = background_scaling
-        self.model.update_parameter(elemental_abundances, density, q_min, q_max, 1.0)
+        self.model.update_parameter(elemental_abundances, density, q_min, q_max, 0, 10, False,
+                                    None, {}, 1.5, 5, 1)
 
         sample_spectrum = data_spectrum - background_scaling * bkg_spectrum
         sq_spectrum_core = calculate_sq(sample_spectrum, density, elemental_abundances)
