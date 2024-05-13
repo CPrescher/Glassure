81efa647ebe6d384835127b16c47c47e2de1b17f
Clemens Prescher <clemens.prescher@gmail.com>
2024-05-13 09:33:53 +0200

    Starting with some ideas

--- glassure/core/methods.py
+++ glassure/core/methods.py
@@ -21,5 +21,24 @@ class FourierTransformMethod(Enum):
     """
     Enum class for the different methods to perform a Fourier transform.
     """
-    FFT = 'fft'
-    INTEGRAL = 'integral'
+
+    FFT = "fft"
+    INTEGRAL = "integral"
+
+
+class ScatteringFactorSource(Enum):
+    """
+    Enum class for the different sources of the scattering factors.
+    """
+
+    HAJDU = "hajdu"
+    BROWN_HUBBELL = "brown_hubbell"
+
+
+class NormalizationFitScaling(Enum):
+    """
+    Enum class for the different scaling methods for the fit.
+    """
+
+    LINEAR = "linear"
+    QUADRATIC = "quadratic"
