d4cb2f9ea3e852418f8fdac45224073e611c16df
Clemens Prescher <clemens.prescher@gmail.com>
2024-05-14 13:50:26 +0200

    refactoring of normalization tests

--- tests/core_tests/test_normalization.py
+++ tests/core_tests/test_normalization.py
@@ -9,6 +9,7 @@ from glassure.core.utility import (
     convert_density_to_atoms_per_cubic_angstrom,
 )
 from glassure.core.normalization import normalize, normalize_fit
+from glassure.core.scattering_factors import calculate_coherent_scattering_factor
 
 from .. import unittest_data_path
 
@@ -16,79 +17,67 @@ data_path = os.path.join(unittest_data_path, "Mg2SiO4_ambient.xy")
 bkg_path = os.path.join(unittest_data_path, "Mg2SiO4_ambient_bkg.xy")
 
 
-def test_normalize():
-    data = Pattern.from_file(data_path)
-    bkg = Pattern.from_file(bkg_path)
+class TestNormalization:
+    def setup_method(self):
+        self.data = Pattern.from_file(data_path)
+        self.bkg = Pattern.from_file(bkg_path)
 
-    sample = data - bkg
-    sample = sample.limit(1, 15)
+        self.sample = self.data - self.bkg
+        self.sample = self.sample.limit(1, 15)
 
-    composition = {"Mg": 2, "Si": 1, "O": 4}
-    density = 2.9
-    atomic_density = convert_density_to_atoms_per_cubic_angstrom(composition, density)
+        self.composition = {"Mg": 2, "Si": 1, "O": 4}
+        self.density = 2.9
+        self.atomic_density = convert_density_to_atoms_per_cubic_angstrom(self.composition, self.density)
 
-    f_squared_mean = calculate_f_squared_mean(composition, sample.x)
-    f_mean_squared = calculate_f_mean_squared(composition, sample.x)
-    incoherent_scattering = calculate_incoherent_scattering(composition, sample.x)
+        self.f_squared_mean = calculate_f_squared_mean(self.composition, self.sample.x)
+        self.f_mean_squared = calculate_f_mean_squared(self.composition, self.sample.x)
+        self.incoherent_scattering = calculate_incoherent_scattering(self.composition, self.sample.x)
 
-    n, _ = normalize(
-        sample, atomic_density, f_squared_mean, f_mean_squared, incoherent_scattering
-    )
-    assert n > 0
-
-
-def test_normalize_fit():
-    data = Pattern.from_file(data_path)
-    bkg = Pattern.from_file(bkg_path)
-
-    sample = data - bkg
-    sample = sample.limit(1, 20)
-
-    composition = {"Mg": 2, "Si": 1, "O": 4}
-
-    f_squared_mean = calculate_f_squared_mean(composition, sample.x)
-    incoherent_scattering = calculate_incoherent_scattering(composition, sample.x)
-
-    params_1, _ = normalize_fit(
-        sample, f_squared_mean, incoherent_scattering, multiple_scattering=False
-    )
-    params_2, _ = normalize_fit(
-        sample, f_squared_mean, incoherent_scattering, multiple_scattering=True
-    )
-    params_3, _ = normalize_fit(
-        sample, f_squared_mean, incoherent_scattering, correct_diamond=True
-    )
-
-    assert params_1["n"].value != params_2["n"].value
-    assert params_1["n"].value != params_3["n"].value
-    assert params_2["multiple"].value > 0
-    assert params_3["n_diamond"].value > 0
-
-    params_4, _ = normalize_fit(
-        sample, f_squared_mean, incoherent_scattering, q_cutoff=5
-    )
-    assert params_4["n"].value != params_1["n"].value
+    def test_normalize(self):
+        n, _ = normalize(
+            self.sample,
+            self.atomic_density,
+            self.f_squared_mean,
+            self.f_mean_squared,
+            self.incoherent_scattering,
+        )
+        assert n > 0
 
 
-def test_compare_normalize_and_normalize_fit():
-    data = Pattern.from_file(data_path)
-    bkg = Pattern.from_file(bkg_path)
+    def test_normalize_fit(self):
+        params_1, _ = normalize_fit(
+            self.sample, self.f_squared_mean, self.incoherent_scattering, multiple_scattering=False
+        )
+        params_2, _ = normalize_fit(
+            self.sample, self.f_squared_mean, self.incoherent_scattering, multiple_scattering=True
+        )
+        diamond_scattering = calculate_coherent_scattering_factor("C", self.sample.x)
+        params_3, _ = normalize_fit(
+            self.sample,
+            self.f_squared_mean,
+            self.incoherent_scattering,
+            container_scattering=diamond_scattering,
+        )
 
-    sample = data - bkg
-    sample = sample.limit(1, 15)
+        assert params_1["n"].value != params_2["n"].value
+        assert params_1["n"].value != params_3["n"].value
+        assert params_2["multiple"].value > 0
+        assert params_3["n_container"].value > 0
 
-    composition = {"Mg": 2, "Si": 1, "O": 4}
-    density = 2.9
-    atomic_density = convert_density_to_atoms_per_cubic_angstrom(composition, density)
+        params_4, _ = normalize_fit(
+            self.sample, self.f_squared_mean, self.incoherent_scattering, q_cutoff=5
+        )
+        assert params_4["n"].value != params_1["n"].value
 
-    f_squared_mean = calculate_f_squared_mean(composition, sample.x)
-    f_mean_squared = calculate_f_mean_squared(composition, sample.x)
-    incoherent_scattering = calculate_incoherent_scattering(composition, sample.x)
-
-    n_normalize, _ = normalize(
-        sample, atomic_density, f_squared_mean, f_mean_squared, incoherent_scattering
-    )
-    p_normalize_fit, _ = normalize_fit(
-        sample, f_squared_mean, incoherent_scattering, q_cutoff=3
-    )
-    assert np.isclose(n_normalize, p_normalize_fit["n"].value, rtol=1e-2)
+    def test_compare_normalize_and_normalize_fit(self):
+        n_normalize, normalized_pattern_1 = normalize(
+            self.sample,
+            self.atomic_density,
+            self.f_squared_mean,
+            self.f_mean_squared,
+            self.incoherent_scattering,
+        )
+        p_normalize_fit, normalized_pattern_2 = normalize_fit(
+            self.sample, self.f_squared_mean, self.incoherent_scattering, q_cutoff=5
+        )
+        assert np.isclose(n_normalize, p_normalize_fit["n"].value, rtol=1e-2)
