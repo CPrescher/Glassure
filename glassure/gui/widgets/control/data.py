277eb4cb95437c7a41320d68f73e6004343c2194
Clemens Prescher <clemens.prescher@gmail.com>
2024-06-03 09:43:25 +0200

    Redesign Data Gui Element

--- glassure/gui/widgets/control/data.py
+++ glassure/gui/widgets/control/data.py
@@ -17,7 +17,6 @@ class DataWidget(QtWidgets.QWidget):
         self._layout.setSpacing(0)
         self._layout.addWidget(self.file_widget)
         self._layout.addWidget(self.background_options_gb)
-        self._layout.addWidget(self.smooth_gb)
         self._layout.addSpacing(5)
 
         self.setLayout(self._layout)
@@ -50,7 +49,7 @@ class FileWidget(QtWidgets.QWidget):
         self.background_btn_layout.addWidget(self.load_background_btn)
         self.background_btn_layout.addWidget(self.reset_background_btn)
 
-        self.vertical_layout.addLayout(self.background_btn_layout)             
+        self.vertical_layout.addLayout(self.background_btn_layout)
         self.vertical_layout.addWidget(self.background_filename_lbl)
 
         self.setLayout(self.vertical_layout)
@@ -77,8 +76,7 @@ class BackgroundOptionsGroupBox(QtWidgets.QGroupBox):
         self.scale_step_txt = FloatLineEdit("0.01")
 
     def style_widgets(self):
-        self.scale_lbl.setAlignment(QtCore.Qt.AlignVCenter |
-                                    QtCore.Qt.AlignRight)
+        self.scale_lbl.setAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignRight)
 
         self.scale_sb.setValue(1.0)
         self.scale_sb.setSingleStep(0.01)
@@ -105,34 +103,3 @@ class BackgroundOptionsGroupBox(QtWidgets.QGroupBox):
 
     def scale_step_changed(self):
         self.scale_sb.setSingleStep(float(str(self.scale_step_txt.text())))
-
-
-class SmoothGroupBox(QtWidgets.QGroupBox):
-    def __init__(self, *args):
-        super(SmoothGroupBox, self).__init__(*args)
-        self.grid_layout = QtWidgets.QGridLayout()
-        self.grid_layout.setContentsMargins(3, 5, 5, 3)
-        self.grid_layout.setSpacing(5)
-
-        self.smooth_lbl = QtWidgets.QLabel("Smooth:")
-        self.smooth_lbl.setAlignment(QtCore.Qt.AlignVCenter |
-                                     QtCore.Qt.AlignRight)
-
-        self.smooth_sb = QtWidgets.QDoubleSpinBox()
-        self.smooth_sb.setAlignment(QtCore.Qt.AlignRight)
-        self.smooth_sb.setSingleStep(1)
-        self.smooth_sb.setMinimumWidth(80)
-
-        self.smooth_step_txt = FloatLineEdit("1")
-        self.smooth_step_txt.setMaximumWidth(60)
-
-        self.smooth_step_txt.editingFinished.connect(self.step_changed)
-
-        self.grid_layout.addWidget(self.smooth_lbl, 0, 0)
-        self.grid_layout.addWidget(self.smooth_sb, 0, 1)
-        self.grid_layout.addWidget(self.smooth_step_txt, 0, 2)
-
-        self.setLayout(self.grid_layout)
-
-    def step_changed(self):
-        self.smooth_sb.setSingleStep(float(str(self.smooth_step_txt.text())))
